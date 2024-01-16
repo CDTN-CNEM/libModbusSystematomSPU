537a248e07e35cfdbff209ee9823592a9eb15a2f
Thalles Campagnani <thallescampagnani@gmail.com>
2024-01-16 19:12:01 -0300

    Create struct SPU_DATA

--- libModbusSystematomSPU.h
+++ libModbusSystematomSPU.h
@@ -26,6 +26,30 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #include <cstdlib>
 #include <cstdint>
 
+struct SPU_DATA
+{
+    bool    READ            = 0;
+    float   N_DATA_FP       = 0;
+    float   T_DATA_FP       = 0;
+    float   F1_DATA_FP      = 0;
+    float   F2_DATA_FP      = 0;
+    float   F3_DATA_FP      = 0;
+    float   EMR_N_THRESHOLD = 0;
+    float   WRN_N_THRESHOLD = 0;
+    float   EMR_T_THRESHOLD = 0;
+    float   WRN_T_THRESHOLD = 0;
+    bool    EMR_N           = 0;
+    bool    WRN_N           = 0;
+    bool    EMR_T           = 0;
+    bool    WRN_T           = 0;
+    bool    R1              = 0;
+    bool    R2              = 0;
+    bool    R3              = 0;
+    bool    RDY             = 0;
+    bool    TEST            = 0;
+    bool    XXXX            = 0;
+};
+
 
 void libModbusSystematomSPU_license();
 
