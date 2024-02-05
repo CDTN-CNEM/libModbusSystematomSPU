923ebed21e9e8915f74b5cd6468f8f64a65c9add
Thalles Campagnani <thallescampagnani@gmail.com>
2024-02-05 17:30:59 -0300

    Add Connection successful

--- libModbusSystematomSPU.cpp
+++ libModbusSystematomSPU.cpp
@@ -41,6 +41,7 @@ libModbusSystematomSPU::libModbusSystematomSPU(std::string portname) : portname(
         std::cerr << "Error: Failed to connect to Modbus device: " << get_portname() << " | Error: "<< modbus_strerror(errno) << std::endl;
         libModbusSystematomSPU::~libModbusSystematomSPU();
     }
+    std::cerr << "Connection successful to: " << get_portname() << std::endl;
 }
 
 libModbusSystematomSPU::~libModbusSystematomSPU() {
