4e1100d599f7f77316b0ecc846586188b972a29a
Álan Crístoffer <acristoffers@startmail.com>
2024-03-26 10:15:38 -0300

    chore: add CMakeLists.txt and Nix files

--- src/test.cpp
+++ src/test.cpp
@@ -17,7 +17,8 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
-#include "libModbusSystematomSPU.h"
+#include <libModbusSystematomSPU.h>
+
 #include <string>
 #include <chrono>
 
